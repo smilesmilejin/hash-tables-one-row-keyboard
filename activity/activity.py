a43e50a211ef1cc3012b9899a8119183dbd46647
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-10-27 13:37:24 -0700

    adds solution

--- activity/activity.py
+++ activity/activity.py
@@ -1,5 +1,14 @@
 def typing_time(layout, word):
-    pass
+    positions = {letter: pos for pos, letter in enumerate(layout)}
+    location = 0
+    typed_time = 0
+
+    for letter in word:
+        next_loc = positions[letter]
+        typed_time += abs(next_loc - location)
+        location = next_loc
+
+    return typed_time
     
 # skipping error handling
 # empty keys? empty word? repeated letters in keys?
